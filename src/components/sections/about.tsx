3b9b9d7d2a1b533ddc37a11efa2ab4ede9901a06
Aki Aoki <akiko.aoki.0w0@gmail.com>
2022-04-12 13:54:32 +0300

    Fixed hhru link

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -56,7 +56,7 @@ export const About = () => {
                         </a>
                     </div>
                     <div className="col-1 d-grid ms-2 me-2">
-                        <a href="hh.ru" target="_blank" data-bs-toggle="tooltip" data-bs-placement="bottom" title="hh.ru">
+                        <a href="https://hh.ru/resume/2e5311f4ff09adf4c70039ed1f51586f497536" target="_blank" data-bs-toggle="tooltip" data-bs-placement="bottom" title="hh.ru">
                             <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" className="bi bi-file-person" viewBox="0 0 16 16">
                                 <path d="M12 1a1 1 0 0 1 1 1v10.755S12 11 8 11s-5 1.755-5 1.755V2a1 1 0 0 1 1-1h8zM4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4z"/>
                                 <path d="M8 10a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
