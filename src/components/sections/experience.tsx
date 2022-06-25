c73f747322a318d38f259bd5b3834da9c28821b9
Aki Aoki <akiko.aoki.0w0@gmail.com>
2022-06-25 13:07:01 +0300

    Added work experience

--- src/components/sections/experience.tsx
+++ src/components/sections/experience.tsx
@@ -17,6 +17,27 @@ export const Experience = () => {
                             </div>
                         </div>
                         <div className="col text-start ms-1 ms-md-0">
+                            <span className="badge bg-success">2022-Current</span>&ensp;
+                            <span className="badge bg-success">ELMA</span>&ensp;
+                            <span className="badge bg-success">Full-time</span>&ensp;
+                            <h3 className="fg-primary">.NET Full-Stack</h3>
+                            <p className="fg-primary">
+                                ELMA BPM Development.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-1 fg-primary justify-content-center p-0 ms-3 ms-md-0">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-building" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M14.763.075A.5.5 0 0 1 15 .5v15a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5V14h-1v1.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V10a.5.5 0 0 1 .342-.474L6 7.64V4.5a.5.5 0 0 1 .276-.447l8-4a.5.5 0 0 1 .487.022zM6 8.694 1 10.36V15h5V8.694zM7 15h2v-1.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5V15h2V1.309l-7 3.5V15z"/>
+                                <path d="M2 11h1v1H2v-1zm2 0h1v1H4v-1zm-2 2h1v1H2v-1zm2 0h1v1H4v-1zm4-4h1v1H8V9zm2 0h1v1h-1V9zm-2 2h1v1H8v-1zm2 0h1v1h-1v-1zm2-2h1v1h-1V9zm0 2h1v1h-1v-1zM8 7h1v1H8V7zm2 0h1v1h-1V7zm2 0h1v1h-1V7zM8 5h1v1H8V5zm2 0h1v1h-1V5zm2 0h1v1h-1V5zm0-2h1v1h-1V3z"/>
+                            </svg>
+                            <div className="d-flex h-100 mt-2 pb-5 justify-content-center">
+                                <div className="vr fg-primary opacity-100"></div>
+                            </div>
+                        </div>
+                        <div className="col text-start ms-1 ms-md-0">
                             <span className="badge bg-success">2021-2022</span>&ensp;
                             <span className="badge bg-success">Matrix Creators</span>&ensp;
                             <span className="badge bg-success">Full-time</span>&ensp;
